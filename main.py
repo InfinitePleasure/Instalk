6613b37fb0e816d7d67886e7c15fab08adfe8e7b
InfinitePleasure <100088002+InfinitePleasure@users.noreply.github.com>
2022-07-19 22:52:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def dl(file, path):
             cl.clip_download(int(mediaa.pk), path)
     elif mediaa.media_type == 8:
         cl.album_download(int(mediaa.pk), path)
-    print("downloaded: " + mediaa.pk)
+    print("downloaded media: " + mediaa.pk)
 
 
 def get_medias(user_to_dl):
@@ -35,6 +35,7 @@ def get_medias(user_to_dl):
         for media in medias:
             if x <= (len(medias) - media_count[user_to_dl.username]):
                 dl(media, path)
+                print(str(x) + "/" + str(len(medias) - media_count[user_to_dl.username]))
                 time.sleep(5)
             x = x + 1
         media_count[user_to_dl.username] = user_to_dl.media_count
@@ -58,6 +59,7 @@ def get_stories(user_to_dl, client):
             if x > story_count[user_to_dl.username]:
                 try:
                     client.story_download(int(story.pk), Path(user_to_dl.username + "/stories/" + story.pk))
+                    print("downloaded story: " + story.pk + " " + str(x) + "/" + str(len(stories)))
                     time.sleep(5)
                 except instagrapi.exceptions.UnknownError:
                     print(client.story_info(int(story.pk)))
@@ -79,23 +81,32 @@ def get_stories(user_to_dl, client):
 
 if __name__ == '__main__':
     cl = Client()
-    cl.login("pseudo", "mdp")
+    verif = input("A2F code verification (google authenticator): ")
+    cl.login("pseudo", "password", verification_code=str(verif))
+
+    print(cl.user_id)
 
     with open("media_count.yml") as f:
         media_count = yaml.safe_load(f)
     with open("story_count.yml") as f:
         story_count = yaml.safe_load(f)
-    usernames = ["pseudos ici"]
+    usernames = ["liste pseudo"]
+    ids = []
+    for username in usernames:
+        ids.append(cl.user_id_from_username(username))
+        if username not in media_count:
+            media_count[username] = 0
+        if username not in story_count:
+            story_count[username] = 0
     while True:
-        for username in usernames:
-            user_id = cl.user_id_from_username(username)
-            get_medias(cl.user_info(user_id))
-            get_stories(cl.user_info(user_id), cl)
+        for id_ in ids:
+            get_medias(cl.user_info(id_))
+            get_stories(cl.user_info(id_), cl)
+            print("data of " + cl.user_info(id_).username + " checked")
         with open('story_count.yml', 'w') as f:
             yaml.dump(story_count, f)
         with open('media_count.yml', 'w') as f:
             yaml.dump(media_count, f)
         time.sleep(15)
-        print("new run")
 
 
